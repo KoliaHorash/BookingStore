e0960662a284ce1bf5dfcb30c7fa30b25a268e07
KoliaHorash <kolia28011994@gmail.com>
2018-07-23 16:44:52 +0300

    don't work connection

---
+++ BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Відьмак 1", Author = "Сапковський" , Price = 98});
+            db.Books.Add(new Book { Name = "Відьмак 2", Author = "Сапковський", Price = 98 });
+            db.Books.Add(new Book { Name = "Відьмак 3", Author = "Сапковський", Price = 98 });
+
+            base.Seed(db);
+        }
+    }
+}
